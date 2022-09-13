bd83518fafeda6572b7f19a271538388c9427d38
Kevin Brand <kevinbrand99@gmail.com>
2022-09-13 15:42:06 +0200

    Finished augmented network

--- src/train_models.py
+++ src/train_models.py
@@ -132,3 +132,48 @@ def train_derotated_standard(x_train, y_train, x_val, y_val, x_test, y_test, run
 
     best_model = keras.models.load_model(f"../../models/derotated_standard_model{run}.h5")
     return best_model.evaluate(x_test, y_test)
+
+def train_augmented_standard(x_train, y_train, x_val, y_val, x_test, y_test, run):
+    """Construct, train and evaluate the standard network on augmented data.
+    
+    Parameters
+    ----------
+    x_train : ndarray
+        The training data.
+    y_train : ndarray
+        The class labels of the training data.
+    x_val : ndarray
+        The validation data.
+    y_val : ndarray
+        The class labels of the validation data.
+    x_test : ndarray
+        The test data.
+    y_test : ndarray
+        The class labels of the test data.
+    run : integer
+        Indicates which training run this is.
+    
+    Returns
+    -------
+    tuple
+        A tuple containing the accuracy and loss of the final network when evaluated on the test set.
+    """
+    probe_dir('../../lr_logs/')
+    probe_dir('../../models/')
+    std_cnn = construct_standard()
+    
+    #Tensorboard setup
+    run_logdir = os.path.join(os.curdir, f"../../lr_logs/standard_aug_run{run}")
+    
+    # Create utility callbacks
+    early_stop = keras.callbacks.EarlyStopping(patience=5)
+    save = keras.callbacks.ModelCheckpoint(f"../../models/aug_standard_model{run}.h5", save_best_only=True)
+    tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
+
+    # Train model
+    train_log = std_cnn.fit(x_train, y_train, epochs=100,
+                   validation_data=(x_val, y_val),
+                   callbacks=[save, tensorboard_cb, early_stop])
+
+    best_model = keras.models.load_model(f"../../models/aug_standard_model{run}.h5")
+    return best_model.evaluate(x_test, y_test)
